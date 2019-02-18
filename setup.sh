09a8291c2c3117de60b0e0dfe9a03f9492725528
Gede Wahyu <tokekbesi@gmail.com>
2019-02-18 11:57:03 +0700

    install fzf & styling prompt

--- setup.sh
+++ setup.sh
@@ -3,4 +3,11 @@
 cp ./bashrc ~/.bashrc
 cp ./bash_aliases ~/.bash_aliases
 cp ./bash_prompt ~/.bash_prompt
+
+# install fzf to improve search 
+if [ ! -f ~/.fzf.bash ]; then
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  ~/.fzf/install
+fi
+
 source ~/.bashrc
