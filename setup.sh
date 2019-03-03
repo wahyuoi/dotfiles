e453be09e22e86f56e34ffedfda6de94ad9f15d6
Gede Wahyu <tokekbesi@gmail.com>
2019-03-04 00:17:27 +0700

    download lambda-mod themes

--- setup.sh
+++ setup.sh
@@ -4,3 +4,6 @@ ln -f -P ./bash/bashrc $HOME/.bashrc
 ln -f -P ./bash/bash_aliases $HOME/.bash_aliases
 ln -f -P ./bash/bash_prompt $HOME/.bash_prompt
 ln -f -P ./zsh/zshrc $HOME/.zshrc
+
+mkdir -p ~/.oh-my-zsh/themes
+wget https://raw.githubusercontent.com/wahyuoi/lambda-mod-zsh-theme/master/lambda-mod.zsh-theme -O ~/.oh-my-zsh/themes/lambda-mod.zsh-theme --quiet
